7fe5ff9257a069529becf436bc96607dac9bcc44
ReeceTait <reece.tait04@gmail.com>
2019-01-02 16:29:38 +0000

    Initial commit, added spdlog and fixed bugs

---
+++ CDA/src/CDA.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by CDA applications
+
+#include "CDA/Application.h"
+#include "CDA/Log.h"
+
+// ---Entry Point --------------------
+#include "CDA/EntryPoint.h"
+//------------------------------------
